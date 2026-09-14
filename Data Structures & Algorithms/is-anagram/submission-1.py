0fbaf932a1c8ba6c42febd4a10663afc3fa64c06
Joseph <91398428+0-Dig1t@users.noreply.github.com>
2026-09-14 13:02:56 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq_s, freq_t = defaultdict(int), defaultdict(int)
+
+        for char in s:
+            freq_s[char] += 1
+        for char in t:
+            freq_t[char] += 1
+
+        return freq_s == freq_t
